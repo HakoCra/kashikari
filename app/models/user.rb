833c44660abd0ab5962ab84e7e85894599ec837e
i544c <leo@i544c.me>
2018-02-10 17:10:06 +0900

    Add sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  after_create :update_access_token!
+
   validates :email, presence: true
+
+  def update_access_token!
+    self.access_token = "#{self.id}:#{Devise.friendly_token}"
+    save
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  namespace :v1, defaults: { format: :json } do
+    resource :login, only: [:create], controller: :sessions
+    resource :users, only: [:create]
+  end
 end
