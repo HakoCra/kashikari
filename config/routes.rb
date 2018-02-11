b84cec63908d32566765a98db9bbf6c705ee57d0
i544c <leo@i544c.me>
2018-02-11 10:32:02 +0900

    Move messages to v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :messages
   devise_for :users
 
   namespace :v1, defaults: { format: :json } do
     resources :beacons
+    resources :messages
     resource :login, only: [:create], controller: :sessions
     resource :users, only: [:create]
   end
