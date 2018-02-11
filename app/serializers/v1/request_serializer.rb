d67bf39c044aa7a8a397f4ee54ff2e157055faec
i544c <leo@i544c.me>
2018-02-12 02:31:25 +0900

    Add accepted_users in Request

--- app/serializers/v1/request_serializer.rb
+++ app/serializers/v1/request_serializer.rb
@@ -2,6 +2,7 @@ module V1
   class RequestSerializer < ActiveModel::Serializer
     attributes :id, :title, :timelimit, :reward, :comment
     has_one :user
+    has_many :accepted_users, class_name: "User"
   end
 
   class UserSerializer < ActiveModel::Serializer
